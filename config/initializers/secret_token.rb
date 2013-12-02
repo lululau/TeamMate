162ca30c3dbb3e73b16d2dfe09916a1f8a7c3eb3
lululau <liuxiang921@gmail.com>
2013-12-03 02:00:44 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamMate::Application.config.secret_key_base = 'ccfb553e846880771e17d8904e8966e7ee21f9c7b4de1bbec60d7fc4d749086311dd8d638c22dea57d12e2a7b206c170d337b94edad3491bc350b432bc6b298e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamMate::Application.config.session_store :cookie_store, key: '_TeamMate_session'
