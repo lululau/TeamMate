6134a1c81e90aeea6dc83940a30d9df26f82fba5
lululau <liuxiang921@gmail.com>
2013-12-29 23:35:28 +0800

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ TeamMate::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.163.com",
+    :port => 25,
+    :domain => "163.com",
+    :authentication => :login,
+    :user_name => "team_mates@163.com", #你的邮箱
+    :password => "applescript921"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
